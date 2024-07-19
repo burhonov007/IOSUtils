bcd17a644c7df81814c39072c7ebaed3dcf04f69
Akmalkhon Burkhonov <akmalburhonov5515@gmail.com>
2024-07-19 08:03:51 +0500

    Initial Commit

---
+++ IOSUtils/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IOSUtils
+//
+//  Created by itserviceimac on 19/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
